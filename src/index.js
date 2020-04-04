9a285aec7e79479abada5a6ccd57dd13f89b5c42
WONHEEJANG <jangwh93@gmail.com>
2020-04-05 00:12:02 +0900

    move

--- src/index.js
+++ src/index.js
@@ -19,12 +19,12 @@ calendar.createSchedules([
 
   {
     id: '2',
-    calendarId: 'General Lecture',
-    title: '건강과 영양',
+    calendarId: 'Major Lecture',
+    title: '건강과 영',
     category: 'time',
     dueDateClass: '',
-    start: '2020-04-01T14:30:00',
-    end: '2020-04-01T16:30:00',
+    start: '2020-04-07T14:30:00',
+    end: '2020-04-07T16:30:00',
     isReadOnly: true // schedule is read-only
   }
 ]);
@@ -34,8 +34,8 @@ calendar.createSchedules([
     calendarId: 'Travel', // calendarId가 바뀌었죠?
     title: '강촌 OT',
     category: 'allday', // 'allday'로 지정합니다
-    start: '2020-04-02',
-    end: '2020-04-02',
+    start: '2020-04-05',
+    end: '2020-04-05',
     color: '#ffffff', // 일정 색상을 직접 지정할 수 있어요
     bgColor: '#03bd9e',
     dragBgColor: '#03bd9e',
@@ -48,8 +48,8 @@ calendar.createSchedules([
     calendarId: 'Major Lecture',
     title: '소프트웨어 개론 레포트 제출',
     category: 'task', // 'task'로 지정합니다
-    start: '2020-04-03T10:30:00',
-    end: '2020-04-03T11:30:00',
+    start: '2020-04-06T10:30:00',
+    end: '2020-04-06T11:30:00',
     color: '#ffffff', // 일정 색상을 직접 지정할 수 있어요
     bgColor: '#9e5fff',
     dragBgColor: '#9e5fff',
@@ -63,8 +63,8 @@ calendar.createSchedules([
     calendarId: 'Homework',
     title: '중간고사 종료',
     category: 'milestone', // 'milestone'으로 지정합니다
-    start: '2020-03-31T10:30:00',
-    end: '2020-03-31T11:30:00',
+    start: '2020-04-08T10:30:00',
+    end: '2020-04-08T11:30:00',
     color: '#bbdc00', // 일정 색상을 직접 지정할 수 있어요
     bgColor: '#ffffff',
     dragBgColor: '#ffffff',
